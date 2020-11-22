44dec7963549d5d66c05f038647052a0826b1304
Gadi Lifshitz <gadilif@gmail.com>
2020-11-22 16:40:07 +0200

    some cleanups

--- big_bang.go
+++ big_bang.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+/******************************************************************************************************************************************
+    In this exercise you will implement a text search engine.
+    You can use one store object with any data structure that suits your idea.
+    The tests are divided into steps, please pass each step at a time, but keep previous steps working.
+    Try to complete as much steps as you can but keep in mind the complexity and quality of your solution.
+    Few tips before you go:
+    We suggest you to read all tests and assert messages before you start implementing, it will save you some time in the advanced steps.
+	Feel free to install and impoort any external package that might help you!
+*******************************************************************************************************************************************/
 
 /*
    Stores the document and have it available to search.
@@ -20,8 +28,4 @@ func index(text string, id int) {
 func match(text string) []int {
 	//fmt.Printf("match(%s)", text)
 	return []int{}
-}
-
-func main() {
-	fmt.Println("This is the Big Bang exercise main. Go test...")
 }
